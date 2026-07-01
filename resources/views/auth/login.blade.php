282d9443c35c3758e5a292c2fc3740cf2bf059ba
Carlos-Yan0 <yan.c.oliveira08@proton.me>
2026-07-01 20:25:08 -0300

    Add login page structure

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,47 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <main class="flex justify-center items-center min-h-screen">
+        <form action="" class="card w-96 bg-base-200 shadow-xl">
+            @csrf
+
+            <div class="card-body h-85 justify-between">
+                <h1 class="card-title text-2xl justify-center mb-4">
+                    Login
+                </h1>
+
+
+                <label class="form-control w-full">
+                    <span class="label-text mb-1">Email</span>
+                    <input
+                        type="email"
+                        placeholder="Example@Email.com"
+                        class="input input-bordered w-full"
+                    >
+                </label>
+
+                <label class="form-control w-full">
+                    <span class="label-text mb-1">Password</span>
+                    <input
+                        type="text"
+                        placeholder="ExamplePassword"
+                        class="input input-bordered w-full"
+                    >
+                </label>
+
+                <button class="btn btn-primary">
+                    Entrar
+                </button>
+
+
+            </div>
+        </form>
+    </main>
+</body>
+</html>
