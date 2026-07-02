bbeb550bd05b6cb59bd76e04cc75284d76ee67ca
Carlos-Yan0 <yan.c.oliveira08@proton.me>
2026-07-02 00:00:01 -0300

    Add Routes for login, logout and registration

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,26 @@
 <?php
 
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\Auth\Register;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/login', 'auth.login');
+
+//Login
+Route::view('/login', 'auth.login')
+    ->middleware('guest');
+Route::post('/login', Login::class);
+
+
+//Register
+Route::view('/register', 'auth.register')
+    ->middleware('guest');
+Route::post('/register', Register::class);
+
+
+//Logout
+Route::get('/logout', Logout::class);
